79e324cf9e9fb06285e241f81f7dec0cc8cb9d5f
Yubing Hou <houyubing24@gmail.com>
2021-10-14 01:24:08 -0500

    php-ls is recognized but not matched

--- elisp/lang-php.el
+++ elisp/lang-php.el
@@ -1,39 +1,21 @@
 (use-package ac-php
   :ensure t)
 
-;; (setq lsp-serenata-php-version 7.4)
-;; (setq lsp-serenata-server-path "~/.emacs.d/vendor/bin/serenata-v5.4.0.phar")
+(use-package lsp-mode
+  :config
+  (setq lsp-prefer-flymake nil)
 
-;; (use-package lsp-mode
-  ;; :config
-  ;; (setq lsp--tcp-server-port 87654) ;; Cake runs at 8765
-  ;; (setq lsp-serenata-php-version 7.4)
-  ;; (setq lsp-serenata-server-path "~/.emacs.d/vendor/bin/serenata-v5.4.0.phar")
-  ;; (add-to-list 'lsp-enabled-clients 'serenata)
- ;; (add-to-list 'lsp-disabled-clients '(php-ls, iph intelephense))
-  ;; (setq lsp-idle-delay 0.300)
-  ;; (setq lsp-log-io nil) ;; improve performance during development, useful for debugging though
-  ;; :hook (php-mode . lsp)
-  ;; :commands (lsp lsp-deferred))
-
+ ; (setq lsp-php-composer-dir "~/.config/composer/")
+  (setq lsp-idle-delay 0.0)
+  :hook (php-mode . lsp)
+  :commands (lsp lsp-deferred))
 
 (use-package php-mode
-  :mode
-  (("\\.php\\'" . php-mode))
+  :ensure t
+  :mode "\\.php\\'"
   :config
-  (add-hook 'php-mode-hook
-	    '(lambda ()
-	       (require 'company-php)
-	       (company-mode t)
-	       (lsp-mode t)
-	       (add-to-list 'company-backends 'company-ac-php-backend)))
-  ;; (setq lsp-serenata-php-version 7.4)
-  ;; (setq lsp-serenata-server-path "~/.emacs.d/vendor/bin/serenata-v5.4.0.phar")
-  ;; (setq lsp--tcp-server-port 87654) ;; Cake runs at 8765
-  ;; (setq lsp-serenata-php-version 7.4)
-  ;; (setq lsp-serenata-server-path "~/.emacs.d/vendor/bin/serenata-v5.4.0.phar")
-  (add-to-list 'lsp-enabled-clients 'php-language-server))
-
-;; (add-hook 'php-mode-hook #'lsp-serenata)
+  (setq lsp-clients-php-server-command "~/.config/composer/vendor/felixfbecker/language-server/bin/php-language-server.php")
+  (add-to-list 'lsp-enabled-clients 'php-ls)
+  (add-to-list 'company-backends 'company-ac-php-backend))
 
 (provide 'lang-php)
